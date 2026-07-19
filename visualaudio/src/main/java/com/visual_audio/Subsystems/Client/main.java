39256cbdc4a6272278852c120cb04d32fb3b923f
Alexandria 156 <asy2678@rit.edu>
2026-07-19 00:50:20 -0400

    Creating main client file

--- visualaudio/src/main/java/com/visual_audio/Subsystems/Client/main.java
+++ visualaudio/src/main/java/com/visual_audio/Subsystems/Client/main.java
@@ -1,5 +1,30 @@
 package com.visual_audio.Subsystems.Client;
 
+import java.io.File;
+
+import javax.sound.sampled.spi.AudioFileReader;
+
 public class main {
-    
+    private AudioFileReader fileData;
+    private File audioFile;
+
+    public boolean uploadAudio(String fileName){
+        return false; 
+    }
+
+    public boolean uploadVideo(String fileName){
+        return false;
+    }
+
+    public boolean verifyFormat(String fileName){
+        return false;
+    }
+
+    public String getFileName(){
+        return null;
+    }
+
+    public String getFileInfo(){
+        return null;
+    }
 }
