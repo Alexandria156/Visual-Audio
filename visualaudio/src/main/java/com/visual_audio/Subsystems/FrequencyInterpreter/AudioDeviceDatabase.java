a29684ec5552212ea3aacd80888db46ffeb64166
Alexandria 156 <asy2678@rit.edu>
2026-07-20 08:57:38 -0400

    Fixed issued with dependencies

--- visualaudio/src/main/java/com/visual_audio/Subsystems/FrequencyInterpreter/AudioDeviceDatabase.java
+++ visualaudio/src/main/java/com/visual_audio/Subsystems/FrequencyInterpreter/AudioDeviceDatabase.java
@@ -1,5 +1,33 @@
 package com.visual_audio.Subsystems.FrequencyInterpreter;
 
 public class AudioDeviceDatabase {
-    
+    private Device[] devices;
+
+    AudioDeviceDatabase() {
+        // Read from a json file
+    }
+
+    public Device[] getTopSounds(SoundProfile profile) {
+        Device[] filterDevices = {};
+
+        return filterDevices;
+    }
+
+    public Device[] getLowPriceSounds(SoundProfile profile) {
+        Device[] filterDevices = {};
+
+        return filterDevices;
+    }
+
+    public Device[] getHighPriceSounds(SoundProfile profile) {
+        Device[] filterDevices = {};
+
+        return filterDevices;
+    }
+
+    public Device[] getCompanySounds(SoundProfile profile, String name) {
+        Device[] filterDevices = {};
+
+        return filterDevices;
+    }
 }
