7cdb69be7e7c74c3a7db5838d7b932e16642fa65
Alexandria 156 <asy2678@rit.edu>
2026-07-20 08:41:39 -0400

    Created device object

--- visualaudio/src/main/java/com/visual_audio/Subsystems/FrequencyInterpreter/Device.java
+++ visualaudio/src/main/java/com/visual_audio/Subsystems/FrequencyInterpreter/Device.java
@@ -1,5 +1,44 @@
 package com.visual_audio.Subsystems.FrequencyInterpreter;
 
 public class Device {
+    private String name;
+    private String deviceType;
+    private SoundProfile soundProfile;
+    private String company;
+    private float msrp;
+
+    Device(String name, String deviceType, SoundProfile soundProfile, String company, float msrp){
+        this.name = name;
+        this.deviceType = deviceType;
+        this.soundProfile = soundProfile;
+        this.company = company;
+        this.msrp = msrp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public SoundProfile getSoundProfile() {
+        return soundProfile;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public float getMsrp() {
+        return msrp;
+    }
+
+    @Override
+    public String toString() {
+        return "Device [name=" + name + ", deviceType=" + deviceType + ", soundProfile=" + soundProfile + ", company="
+                + company + ", msrp=" + msrp + "]";
+    }
     
 }
