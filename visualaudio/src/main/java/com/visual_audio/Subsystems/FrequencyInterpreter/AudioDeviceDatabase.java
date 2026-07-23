f1dae54774ea34eb2dd921efdce06a9e8f5d2c45
Alexandria 156 <asy2678@rit.edu>
2026-07-23 10:57:49 -0400

    Updated dependencies and creating constructor for audio database

--- visualaudio/src/main/java/com/visual_audio/Subsystems/FrequencyInterpreter/AudioDeviceDatabase.java
+++ visualaudio/src/main/java/com/visual_audio/Subsystems/FrequencyInterpreter/AudioDeviceDatabase.java
@@ -1,10 +1,22 @@
 package com.visual_audio.Subsystems.FrequencyInterpreter;
 
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class AudioDeviceDatabase {
     private Device[] devices;
 
     AudioDeviceDatabase() {
         // Read from a json file
+        ObjectMapper map = new ObjectMapper();
+        try {
+            File deviceFile = new File("./visualaudio/src/data/devices.json");
+            this.devices = map.readValue(deviceFile, Device[].class);
+        } catch (Exception e) {
+            System.out.println("Could not load in device database.");
+            e.printStackTrace();
+        }
     }
 
     public Device[] getTopSounds(SoundProfile profile) {
