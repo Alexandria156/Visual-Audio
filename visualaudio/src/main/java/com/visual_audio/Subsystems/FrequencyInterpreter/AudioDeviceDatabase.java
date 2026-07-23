a90241cab29a466d91b872b18b2deb13f93431dc
Alexandria 156 <asy2678@rit.edu>
2026-07-23 11:13:26 -0400

    Added method to get top sound profiles

--- visualaudio/src/main/java/com/visual_audio/Subsystems/FrequencyInterpreter/AudioDeviceDatabase.java
+++ visualaudio/src/main/java/com/visual_audio/Subsystems/FrequencyInterpreter/AudioDeviceDatabase.java
@@ -1,28 +1,34 @@
 package com.visual_audio.Subsystems.FrequencyInterpreter;
 
 import java.io.File;
+import java.util.ArrayList;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class AudioDeviceDatabase {
-    private Device[] devices;
+    private ArrayList<Device> devices;
 
     AudioDeviceDatabase() {
         // Read from a json file
         ObjectMapper map = new ObjectMapper();
         try {
             File deviceFile = new File("./visualaudio/src/data/devices.json");
-            this.devices = map.readValue(deviceFile, Device[].class);
+            this.devices = map.readValue(deviceFile, new TypeReference<ArrayList<Device>>(){});
         } catch (Exception e) {
             System.out.println("Could not load in device database.");
             e.printStackTrace();
         }
     }
 
-    public Device[] getTopSounds(SoundProfile profile) {
-        Device[] filterDevices = {};
-
-        return filterDevices;
+    public ArrayList<Device> getTopSounds(SoundProfile profile) {
+        ArrayList<Device> topDevices = new ArrayList<Device>();
+        devices.forEach(d ->{
+            if(d.getSoundProfile().equals(profile)){
+                topDevices.add(d);
+            }
+        });
+        return topDevices;
     }
 
     public Device[] getLowPriceSounds(SoundProfile profile) {
