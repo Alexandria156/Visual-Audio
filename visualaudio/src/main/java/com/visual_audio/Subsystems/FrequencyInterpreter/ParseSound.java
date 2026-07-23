bc05bec19e9454a3b096204d1be2fef5f75b7504
Alexandria 156 <asy2678@rit.edu>
2026-07-23 11:27:09 -0400

    Began on parse audio class

--- visualaudio/src/main/java/com/visual_audio/Subsystems/FrequencyInterpreter/ParseSound.java
+++ visualaudio/src/main/java/com/visual_audio/Subsystems/FrequencyInterpreter/ParseSound.java
@@ -1,5 +1,27 @@
 package com.visual_audio.Subsystems.FrequencyInterpreter;
 
+import java.io.File;
+
 public class ParseSound {
-    
+    private File audioFile;
+    private int commonLowHz;
+    private int commonHighHz;
+    private int commonMidHz;
+
+    public ParseSound(File audioFile){
+        this.audioFile = audioFile;
+    }
+
+    public void changeFile(File newFile){
+        try {
+            this.audioFile = newFile;
+        } catch (Exception e) {
+            System.out.println("Could not update file.");
+            e.printStackTrace();
+        }
+    }
+
+    public void getAverageLowHz(){
+        
+    }
 }
