8457291e42bc1cd35c095eca1daa93987573611d
Alexandria 156 <asy2678@rit.edu>
2026-07-25 19:17:41 -0400

    Added converter

--- visualaudio/src/main/java/com/visual_audio/Subsystems/FrequencyInterpreter/ParseSound.java
+++ visualaudio/src/main/java/com/visual_audio/Subsystems/FrequencyInterpreter/ParseSound.java
@@ -21,7 +21,19 @@ public class ParseSound {
         }
     }
 
-    public void getAverageLowHz(){
+    //method to convert the byte arrays extracted from uploaded audio into  double arrays
+    public double[] byteToDouble(byte[] audioData){
+        double[] sampledAud = new double[audioData.length/2];
         
+        for(int i = 0, p = 0; i < audioData.length -1; i += 2, p++){
+
+            int sampleInt = (audioData[i] << 8) | (audioData[i+1] & 0xff);
+            sampledAud[p] = sampleInt / 32768.0;
+        }
+
+        return sampledAud;
+    }
+    public void getAverageLowHz(){
+
     }
 }
